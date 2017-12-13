4bf12745f8ec2dde75cf91934dacfc2262c83dda
The Tien <tiendt2311@gmail.com>
2017-12-13 12:06:44 +0700

    classification

--- app/helpers/clustering_helper.rb
+++ app/helpers/clustering_helper.rb
@@ -1,2 +1,5 @@
 module ClusteringHelper
+  def cluste_option
+    [['Very positive', 'very_positive'], ['Positive', 'positive'], ['Negative', 'negative'], ['Very negative', 'very_negative']]
+  end
 end
