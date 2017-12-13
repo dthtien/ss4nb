5e98873c671cb46dfeab3ec10489ffc2dbc16637
The Tien <tiendt2311@gmail.com>
2017-12-13 09:47:39 +0700

    heroku config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,6 @@
-# Sidekiq.configure_server do |config|
-#   config.redis = { url: 'redis://h:p64bbd3af493c40a95928c20d9366fc95a276f6a500e051b8beb851596e8da27d@ec2-34-203-71-78.compute-1.amazonaws.com:12889' }
-# end
-
-# Sidekiq.configure_client do |config|
-#   config.redis = { url: 'redis://h:p64bbd3af493c40a95928c20d9366fc95a276f6a500e051b8beb851596e8da27d@ec2-34-203-71-78.compute-1.amazonaws.com:12889' }
-# end
+Sidekiq.configure_client do |config|
+  config.redis = { size: 5 }
+end
+Sidekiq.configure_server do |config|
+  #no redis settings
+end
